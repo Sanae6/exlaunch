e294830609026d877765ba12ea08c9c0ce410dea
Sanae <atmatm6000@outlook.com>
2022-01-18 10:06:18 -0600

    grr

--- source/program/cool/customPlayer.cpp
+++ source/program/cool/customPlayer.cpp
@@ -1,5 +1,9 @@
 #include "customPlayer.hpp"
 
 namespace ca {
+    BirdPlayer::BirdPlayer(const char* name) : PlayerActorBase(name) {}
 
+    void BirdPlayer::initPlayer(const al::ActorInitInfo&, const PlayerInitInfo&) {
+        
+    }
 }
